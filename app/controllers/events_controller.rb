c8ae00c188ea7dda920840928286776bc0c86661
Nicollas Isabel <nicollasisabel@hotmail.com>
2019-03-09 16:09:56 -0300

    Created events endpoints and some initial views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,33 @@
 class EventsController < ApplicationController
+  before_action :find_event, except: %i[index new create]
+
   def index
     @events = Event.all
+  end
+
+  def show; end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    event = Event.new(event_params)
+
+    if event.save
+      redirect_to event
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :start_time)
+  end
+
+  def find_event
+    @event = Event.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_08_212638) do
+ActiveRecord::Schema.define(version: 2019_03_09_184621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_03_08_212638) do
     t.datetime "start_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
